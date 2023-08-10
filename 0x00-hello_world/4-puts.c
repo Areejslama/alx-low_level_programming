022fb945c72d1e74892b2ab8b555067baf2279e8
Areejslama <Areejslama@github.com>
2023-08-10 16:02:14 -0700

    put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
-
-/*
- * main-this is an entry point
- * @parameters:no parameters
- * Return:0 always
+/**
+ *main-this is an entry point
+ *Description: a C program that prints line with puts function
+ *Return:0 Always (success)
  */
 int main(void)
-{
+{	
+
 	puts("\"Programming is like building a multilingual puzzle);
-	return 0;
+	return(0);
 }	
