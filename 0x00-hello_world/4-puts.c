44068b1818a16164917f9ec4d4ca7c72e5772e3e
Areejslama <Areejslama@github.com>
2023-08-10 14:56:14 -0700

    put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 
+/*
+ * main-entry point
+ * Description: a C program that prints exactly with puts function
+ * Return value:0
+*/
 int main(void)
-{
-      puts("Programming is like building a multilingual puzzle\n");
+{	
+      puts("Programming is like building a multilingual puzzle\n);
        return 0;
 }
