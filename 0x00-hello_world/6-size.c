269648605b08188cae385ea63e91f94520e76878
Areejslama <Areejslama@github.com>
2023-08-10 18:22:05 -0700

    size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main-this is entry point
+ *
+ * Description:A c program that prints the size of various types 
+ *
+ * Return:0 Always (success)
+ */
+int main(void)
+{
+	printf("%c\n",sizeof(char));
+	printf("%d%i\n",sizeof(int));
+	printf("%ld%li\n",sizeof(long int));
+	printf("%lld%lli\n",sizeof(long long int));
+	printf("%f\n",sizeof(float));
+	Return (0);
+}
