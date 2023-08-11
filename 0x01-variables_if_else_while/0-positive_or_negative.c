0414d9abf3df4246fb797b0fed1b8543f4ee4126
Areejslama <Areejslama@github.com>
2023-08-11 07:41:43 -0700

    value

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+/**
+ * main-this is entry point
+ *
+ * Description:program will assign a random number
+ *
+ * Return:0 Always (success)
+ */
+int main(void)
+{
+	int x;
+	if(x>0){
+	printf("The number is positive\n");
+	}
+	else if (x=0)
+	printf("The number is zero\n");
+	else {
+	if(x<0)
+	printf("The number is False\n");
+	return (0);
+}
