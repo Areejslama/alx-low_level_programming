b941d35e9596c334794229493fe4db90cb762667
Areejslama <Areejslama@github.com>
2023-08-10 17:08:45 -0700

    print

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main-this is entry point
+ *
+ * Description: A c program that prints with printf function
+ *
+ * Return:0 Always(success)
+ */
+int main(void)
+{
+	printf("\"with proper grammar, but the outcome is a piece of art,);
+	return (0);
+}	
