1ef5c800dce0d51d8ab7d90456477e6c24c2d99e
Areejslama <Areejslama@github.com>
2023-08-11 11:31:25 -0700

    value

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -7,13 +7,14 @@
  * Description: program will assign a random numbe
  *
  * Return:0 Always (success)
-*/ 
+ */ 
 int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+
 	if (n > 0)	
 	{
 		printf("98 is positive\n");
