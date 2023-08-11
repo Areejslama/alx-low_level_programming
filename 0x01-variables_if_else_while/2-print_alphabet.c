0d9478db89cabc7a3f20b8fa2c46a513dbf31655
Areejslama <Areejslama@github.com>
2023-08-11 15:07:57 -0700

    Alphapet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/**
+ * main-this is entry point
+ *
+ * Description: program that prints the alphabet
+ *
+ * Return:0 Always (success)
+*/
+int main (void)
+{
+	char alphabet = "a";
+	while ( alphabet ="z");
+	{
+		putchar(alphabet);
+	alphabet++;
+	}
+	putchar("\n");
+	return (0);
+}	
