50119913bad617bdacb200718c1c3cb15bb9e251
Areejslama <Areejslama@github.com>
2023-08-11 16:51:50 -0700

    Alphapet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,12 +8,13 @@
 */
 int main (void)
 {
-	char ch;
-	/*prints a-z*/
+	// Get the character to be written
+	char ch='a'
+	 // Write the Character to stdout    
 	for ( ch = 'a'; ch <= 'z' ; ch++)
-	{		
+			
 		putchar(ch);
-	}
+	
 	putchar("\n")
 	return (0);
 }	
