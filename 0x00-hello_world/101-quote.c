ef3f21e9a6e0330cda84575c901673ee92c1bdb4
Areejslama <Areejslama@github.com>
2023-08-12 06:56:49 -0700

    art

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/**
+ * main- this is entry point
+ *
+ * Description:a C program that prints 
+ *
+ * Return: 1 Always (success)
+*/
+int main(void)
+{
+	printf("and that piece of art is useful" - Dora Korpar, 2015-10-19,"\n");
+	return (1);
