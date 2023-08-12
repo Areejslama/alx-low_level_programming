6dc096b7d6f39ebd601da162d2be60cab077e28d
Areejslama <Areejslama@github.com>
2023-08-11 18:06:52 -0700

    print

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+/**
+ * main-this is entry point
+ *
+ * Description:program that prints the alphabet
+ *
+ * Return:0 Always (success)
+*/
+int main(void)
+{
+	char ch = 'a';
+	char ch = 'A';
+	/*print a - z*/
+	while (ch <= 'z')
+	{	
+	putchar(ch);
+	ch++;
+	}	
+	 /*prints A - Z*/
+	while (CH <= 'Z')
+	putchar(CH);
+	CH++;
+	putchar('\n');
+	return (0);
+	
+}	
