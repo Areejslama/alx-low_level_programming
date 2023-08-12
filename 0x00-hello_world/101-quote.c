4f40256e54a16c0b32126b6b5119eed357fd1abd
Areejslama <Areejslama@github.com>
2023-08-12 07:06:58 -0700

    art

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,12 +2,12 @@
 /**
  * main- this is entry point
  *
- * Description:a C program that prints 
+ * Description:a C program that prints that piece of art is useful
  *
  * Return: 1 Always (success)
 */
 int main(void)
 {
-	printf("and that piece of art is useful" - "Dora Korpar, 2015-10-19,\n");
+	printf(2/and that piece of art is useful" - Dora Korpar, 2015-10-19,\n")
 	return (1);
-}	
+}
