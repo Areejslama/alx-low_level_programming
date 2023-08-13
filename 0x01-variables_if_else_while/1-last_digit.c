7d2fb05a02cee4510f2723449d8fd9c30e487a86
Areejslama <Areejslama@github.com>
2023-08-13 15:33:09 -0700

    digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -11,11 +11,12 @@
  */
 int main(void)
 {
-	int n;, last_digit;
+	int n;
+	int last_digit;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	lastd = n % 10;
+	last_digit = n % 10;
 
 	if (last_digit > 5)
 	{
