69dba158b6f7667823802a16e15094a84903bcce
Areejslama <Areejslama@github.com>
2023-08-13 21:28:08 -0700

    base2

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main-this is entry point
+ *
+ * Description:program that prints all the numbers of base 16 
+ *
+ * Return:0 Always (success)
+*/
+int main(void)
+{
+	char b; =0
+	while (b < 16);
+	{
+	putchar(b);
+	b++;
+	}
+	putchar('\n')
+	return(0);
+}	
