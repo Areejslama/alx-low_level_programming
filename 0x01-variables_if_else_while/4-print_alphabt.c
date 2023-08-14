f66c27795517c5fa8a251e770f9037bdf07bce18
Areejslama <Areejslama@github.com>
2023-08-14 16:52:57 -0700

    print

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,13 +9,14 @@
 int main(void)
 {
 	char ch = 'a';
+
 	while (ch <= 'z')
 	{
-		if(ch == 'e' || ch == 'q')
+		if (ch == 'e' || ch == 'q')
 			ch++;
 		putchar(ch);
 		ch++;
 	}
 	putchar('\n');
 	return (0);
-}	
+}
