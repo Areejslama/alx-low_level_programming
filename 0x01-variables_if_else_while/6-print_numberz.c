2324bd4712741cebe8c5a4830a8d4d2f504acd24
Areejslama <Areejslama@github.com>
2023-08-13 23:32:36 -0700

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main-this is entry point
+ *
+ * Description: program that prints all single digit numbers
+ *
+ * Return :0 Always (success)
+*/
+int main(void)
+{
+
+	int num =0
+		while (num < 10)
+		{
+		/*convert num ASCII representation*/
+		putchar(num + '0');
+		num++;
+		}
+		putchar('\n');
+		return(0);
+}		
