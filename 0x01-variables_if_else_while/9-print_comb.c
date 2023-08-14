22a71545831b2cf4a869978046b0f6c2326c2dce
Areejslama <Areejslama@github.com>
2023-08-14 16:04:30 -0700

    comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,13 +12,14 @@ int main(void)
 
 	while ( a < 10)
 	{	
-	putchar(a +'0');
-	a++;
+	putchar(a + '0');
 
 	if (a != 9)
 	{	
 	putchar(' ');
 	putchar(',');
+	}
+	a++;
 	}
 	putchar('\n');
 	return (0);
