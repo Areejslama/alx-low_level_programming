e29f84b05344a67248ced2e043091e27402a086e
Areejslama <Areejslama@github.com>
2023-08-13 18:37:53 -0700

    base

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main-this is entry point
+ *
+ * Description: program that prints the alphabet
+ *
+ * Return :0 Always (success)
+*/
+int main(void)
+{
+	char ch = 'a'
+	/*prints a-z*/
+	while (ch <='a');
+	{
+		putchar(ch);
+		ch++
+		grep -v eq
+	}
+	putchar('\n');
+	return (0);
+	
+}	
---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main-this is entry point
+ *
+ * Description:prints all single digit numbers of base 10
+ *
+ * Return:0 Always (success)
+*/
+int main(void)
+{
+	int count =1;
+
+	while (count <10)
+	{
+		printf ("%d",count);
+		count++;
+	}
+	return (0);
+}			
