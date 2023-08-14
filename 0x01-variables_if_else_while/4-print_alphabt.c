2d25a58a67b9dd4480da7b081ef90e8b1f69cea5
Areejslama <Areejslama@github.com>
2023-08-14 00:19:47 -0700

    print

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -2,21 +2,21 @@
 /**
  * main-this is entry point
  *
- * Description: program that prints the alphabet
+ * Description: a program that prints the alphabet
  *
  * Return :0 Always (sucess)
 */
 int main(void)
 {
-        char ch = 'a';
+	char ch = 'a';
 
-        while(ch <= 'z')
-        {
-	if (ch == 'e' || ch == 'q')
-        ch++;
-        putchar(ch);
-  	ch++;
-        }
-        putchar('\n');
-        return (0);
-}
+	while (ch <= 'z')
+	{
+		 if (ch == 'e' || ch == 'q')
+			 ch++;
+		 putchar(ch);
+		 ch++;
+	}
+		 putchar('\n');
+		 return (0);
+}	 	 
