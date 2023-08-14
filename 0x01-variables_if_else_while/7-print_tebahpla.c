b09ba573f7dd6b2c6fe51f3117461ecac8f9e846
Areejslama <Areejslama@github.com>
2023-08-13 23:55:49 -0700

    rev

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main-this is entry point
+ *
+ * Description:program that prints the lowercase alphabet 
+ *
+ * Return:0 Always (success)
+*/
+int main(void)
+{
+	char ch = 'a';
+	while (ch >= 'z')
+	{
+	putchar(ch);
+	ch--;
+	}
+	putchar('\n')
+	return(0);
+}	
