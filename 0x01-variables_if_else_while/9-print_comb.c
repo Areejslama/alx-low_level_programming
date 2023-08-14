1fb9b57c555f2d663b3a7bbf693ac2413b2304b0
Areejslama <Areejslama@github.com>
2023-08-14 13:22:49 -0700

    comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main-thi is entry point
+ *
+ * Description: a program that prints all possible combinations of digit numbers
+ *
+ * Return:0 Always (success)
+*/
+int main(void)
+{
+	int a = 0;
+	while (a < 10)
+	{	
+	putchar('a',);
+	a++;
+	}
+	putchar('\n');
+	return (0);
+}	
