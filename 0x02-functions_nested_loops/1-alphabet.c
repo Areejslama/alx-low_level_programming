0760f43ef395e4276d49e6d2788d14c956ef518b
Areejslama <Areejslama@github.com>
2023-08-15 09:45:15 -0700

    alpha

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main -this is entry point
+ *
+ * Description: function that prints the alphabet in lowercase
+ *
+*/
+void print_alphabet(void);
+{
+	 char ch = 'a';
+        /*prints a - z*/
+        while (ch <= 'z')
+        {
+        _putchar(ch);
+        ch++;
+        }
+        _putchar ('\n');
+}	
