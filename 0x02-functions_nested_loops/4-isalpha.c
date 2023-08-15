c9efe27b6404e27a2ff2ad3e428e2c23ea5187e1
Areejslama <Areejslama@github.com>
2023-08-15 16:17:50 -0700

    upper

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -4,13 +4,13 @@
  * _isalpha - checks for alphabetic character
  *@c:case must be checked
  *
- *return:1 if a letter lowercase or uppercase and 0 if not
+ *return:1 if c a letter lowercase or uppercase and 0 if not
 */
 int _isalpha(int c)
 {
-	if ((c >= 65 && c <= 90) || if (c >= 90 && c <= 122))
-	{
-	return (1);
-	}
+	if (c >= 97 && c <= 122)
+		return (1);
+	if (c >= 65 && c <= 90)
+		return (1);
 	return (0);
 }
