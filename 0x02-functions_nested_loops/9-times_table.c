3e2f3ec99fbe6d0c2532cba3f2bee648210f0d3c
Areejslama <Areejslama@github.com>
2023-08-16 12:04:55 -0700

    table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -27,7 +27,7 @@ void times_table(void)
                                 _putchar(' ');
                                 _putchar(' ');
                                 _putchar(prod + '0');
-			else
+			else if
 			{	
                                 _putchar(',');
                                 _putchar(' ');
