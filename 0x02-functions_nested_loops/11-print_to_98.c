814219fb7e6dc03463a08ceb01461116ca2e16f1
Areejslama <Areejslama@github.com>
2023-08-16 15:27:53 -0700

    98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,7 +10,7 @@ void print_to_98(int n)
 {
 	int count;
 
-	for (count = n ; count > 98 ; count--)
+	for(count = n ; count > 98 ; count--)
 		printf ("%d, ",  count);
 
 	for (count = n ; count < 98 ; count++)
