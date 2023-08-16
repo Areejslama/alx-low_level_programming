1b6e9d962e6e7ff0504f8b5a293a2f7604c17554
Areejslama <Areejslama@github.com>
2023-08-16 09:03:21 -0700

    hour

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -13,14 +13,14 @@ void jack_bauer(void)
 
 	for(hr = 0 ; hr <= 23 ; hr++)
 	{
-	for(min = 0 ; min <= 59 ; min++)
-	{	
-	_putchar(hr / 10 + 48); 	
-	_putchar(hr % 10 + 48);	
-	_putchar(':');
+		for(min = 0 ; min <= 59 ; min++)
+		{
+		_putchar(hr / 10 + 48); 	
+		_putchar(hr % 10 + 48);	
+		_putchar(':');
+		_putchar(min / 10 + 48); 
+		_putchar(min % 10 + 48);
+		_putchar('\n');
+		}
 	}
-	_putchar(min / 10 + 48); 
-       _putchar(min % 10 + 48);
-	_putchar('\n');
-	}
-}
+}	
