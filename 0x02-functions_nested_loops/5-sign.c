14f873c90f417e0304cd15a55694346e7af61bbb
Areejslama <Areejslama@github.com>
2023-08-15 17:41:24 -0700

    sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,7 +2,7 @@
 #include "main.h"
 /**
  * print_sign -that prints the sign of a number
- * @n:sign must be printed
+ * @n:number must be printed
  *
  * Return:1 if greater than zero and 0 if zero and -1 if less than zero
 */
@@ -18,7 +18,7 @@ int print_sign(int n)
 	_putchar('-');
 	return (-1);
 	}
-	if(n == 0)
+	else(n == 0)
 	{	
 	_putchar('0');
 	return (0);
