bc67bba4bafc794e5353b79d353cb219ed2b23de
Areejslama <Areejslama@github.com>
2023-08-16 07:52:04 -0700

    hour

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,28 +1,23 @@
 #include <stdio.h>
 #include "main.h"
-
-/** main - this is entry point
- *
+/**
  * Description: a function that prints every minute of the day
- *
- * Return:every minute of the day of Jack Bauer
 */
 
 void jack_bauer(void)
 {
-	int hr = 0;
-	int min = 0;
-
-	if (hr < 24)
+	int hr
+	int min 
+	for(hr = 0 ; hr <= 23 ; hr++)
 	{	
-	_putchar(hr / 10 + '0'); ,_putchar(hr % 10 + '0');
-	hr++;
+	_putchar(hr / 10 + '0'); 
+	_putchar(hr % 10 + '0');
 	_putchar(':');
 	}
-	else if (min < 60) 
-	{
-	_putchar(min / 10 + '0'); ,_putchar(min % 10 + '0');
-	min++;
+       for(min = 0 ; min <= 59 ; min++)
+       {       
+	_putchar(min / 10 + '0'); 
+       _putchar(min % 10 + '0');
 	_putchar('\n');
 	}
 }
