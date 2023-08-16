12fb2ce57b9cbd30568cd53a0a046082f758d6a2
Areejslama <Areejslama@github.com>
2023-08-16 14:41:48 -0700

    98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_to_98- prints all numbers to 98
+ * @n:number sould be printed
+ *
+ * Return:numbers
+*/
+void print_to_98(int n)
+{
+	int count;
+	for(count = n ; count > 98 ; count--)
+		printf(%d,count);
+	for(count = n ; count < 98 ; count ++)
+		printf(%d, count);
+	printf("98\n");
+}
