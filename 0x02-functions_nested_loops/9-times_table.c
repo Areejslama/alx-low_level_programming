e206dbbf9c139aee9d73b18ed4d042c789ed790b
Areejslama <Areejslama@github.com>
2023-08-16 12:23:46 -0700

    table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,26 +10,27 @@ void times_table(void)
 	int n , t, prod;
 
 	for (n = 0 ; n <= 9 ; n++)
-	{	
+	{
+		_putchar('0');
 		for (t = 1 ; t <= 9 ; t++)
 		{
-			prod = n * t;
-			 if (t == 0)
-                        {
-                                _putchar('0');
-                        } else if (prod <= 9)
-                        {
-                                _putchar(',');
-                                _putchar(' ');
-                                _putchar(' ');
-                                _putchar(prod + '0');
-                        } else
-                        {
-                                _putchar(',');
-                                _putchar(' ');
-                                _putchar(prod / 10 + '0');
-                                _putchar(prod % 10 + '0');
-                        }
+		prod = n * t;
+		if (t == 0)
+                {
+		_putchar('0');
+		} else if (prod <= 9)
+		{
+		_putchar(',');
+		_putchar(' ');
+                _putchar(' ');
+		_putchar(prod + '0');
+		} else
+		{
+		_putchar(',');
+                _putchar(' ');
+		_putchar(prod / 10 + '0');
+		_putchar(prod % 10 + '0');
+                }
                 }
                 _putchar('\n');
         }
