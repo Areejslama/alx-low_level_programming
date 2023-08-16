72a5dbe563614539c3a9c5aafe3810f3d2b46bd5
Areejslama <Areejslama@github.com>
2023-08-15 18:45:26 -0700

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * _abs -  computes the absolute value of an integer
+ * @int:integer must be computes
+ *
+ * Return:absolute value
+*/
+int _abs(int)
+{
+	if ( i >= 0)
+	{
+	return(i);
+	}
+	else
+	return (-i);
+}	
