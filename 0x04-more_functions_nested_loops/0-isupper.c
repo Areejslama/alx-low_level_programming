74680d93abd89b073dceafb6bb3d1d262946e816
Areejslama <Areejslama@github.com>
2023-08-17 16:47:18 -0700

    letter

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -6,10 +6,9 @@
  *
  * Return:1 if c is uppercase 0 if not
 */
-
 int _isupper(int c)
 {
-	if (c >= 'A' && c <= 'Z')
+	if (c >= 65 && c <= 90)
 	{
 		return (1);
 	}
@@ -17,6 +16,4 @@ int _isupper(int c)
 	{
 		return (0);
 	}
-
-	_putchar('\n');
 }
