3fa90d444162d2f6a99927965979c6ae14cefce2
Areejslama <Areejslama@github.com>
2023-08-17 18:48:58 -0700

    numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_numbers- prints the numbers from 0 to 9
+ *
+ * Return:printed numbers
+*/
+void print_numbers(void)
+{
+	int num = 0;
+
+	if (num <= 57)
+	{
+	_putchar(num);
+	}
+	putchar('\n');
+}
