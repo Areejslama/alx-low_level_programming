c96bb716ae2289d6a5102fb4603decd2cb94785a
Areejslama <Areejslama@github.com>
2023-08-18 11:34:30 -0700

    line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -10,15 +10,15 @@ void print_line(int n)
 {
 	int l;
 
-	if (n <=0)
+	if (n <= 0)
 	{
 		_putchar('\n');
 	}
 	else
 	{
-		for (l = 0 ; l >= n ; l++)
+		for (l = 1 ; l <= n ; l++)
 			
 		_putchar('_');
 	}
 	_putchar('\n');
-}
+}i
