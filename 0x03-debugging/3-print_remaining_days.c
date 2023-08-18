3ae5038a5b2ad3d17069b7abbf6c7ac7ab5ef46c
Areejslama <Areejslama@github.com>
2023-08-17 19:36:51 -0700

    days

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -12,10 +12,14 @@
 
 void print_remaining_days(int month, int day, int year)
 {
-	if ((year % 400 == 0 && year % 100 != 0) || (year % 4 == 0))
+	/**
+	 * leap year when it can be devided
+	 * by 100 and 400 or by 4 evenly
+	 */
+	if ((year % 400 == 0 && year % 100 == 0) || (year % 4 == 0))
 	{
 
-		if (month > 2 && day > 60)
+		if (month > 2 && day >= 60)
 		{
 			day++;
 		}
