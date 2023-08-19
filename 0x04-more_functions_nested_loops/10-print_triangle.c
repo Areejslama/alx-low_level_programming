b527cd48e932ab8951f1a7d0996f52c97f53a35b
Areejslama <Areejslama@github.com>
2023-08-18 17:48:27 -0700

    triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -8,19 +8,21 @@
 */
 void print_triangle(int size)
 {
-    int a;
+	int a;
         int b;
 
         if (size <= 0)
         {
-                _putchar('\n');
+	       	_putchar('\n');
         }
         else
         {
                 for (a = 0 ; a < size ; a++)
                 {
                         for (b = 0 ; b < size ; b++)
-                                _putchar('#');
+			{	
+			       	_putchar('#');
+			}
                         _putchar('\n');
                 }
         }
