080dbf9cf491d3bf568ab6fe9ce31d8fa245ca37
Areejslama <Areejslama@github.com>
2023-08-21 13:10:05 -0700

    rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_rev- that prints a string in reverse
+ * @s:parameter
+ *
+ * Return:0 Always
+*/
+void print_rev(char *s)
+{
+	for (; *s != 0 ; s++)
+	{
+		rev *s;
+	}
+	_putchar('\n');
+}
