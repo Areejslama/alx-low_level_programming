eeef62753575bac10b1038fb3b9714e03c5dd08e
Areejslama <Areejslama@github.com>
2023-08-21 15:16:43 -0700

    rev

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,15 +8,15 @@
 */
 void print_rev(char *s)
 {
-	int a;
+	int a = 0;
 
-	while(s[a] != '\0')
-	{	
-	      a++;
-	}
-	for (c -= 1 ; c >= 0 ; c--)
+	while (s[a]!= '\0')
 	{
-		_putchar(s[c]);
+		a++;
+	}
+	for (a -= 1 ; a >= 0 ; a--)
+	{
+		_putchar(s[a]);
 	}
 	_putchar('\n');
 }
