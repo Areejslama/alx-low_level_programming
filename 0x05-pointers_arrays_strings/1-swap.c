3edfd034c0c5710288b749d280f10d0891ade098
Areejslama <Areejslama@github.com>
2023-08-21 05:57:48 -0700

    98

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * swap_int-  that swaps the values of two integers
+ * @a:an integer
+ * @b:an integer
+ *
+ * Return:swap value
+*/
+void swap_int(int *a, int *b)
+{
+	int *a = 98;
+	int *b = 42
