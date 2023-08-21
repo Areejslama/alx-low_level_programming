3462cfa4343868e2629cca0af1f2eae01944e793
Areejslama <Areejslama@github.com>
2023-08-21 16:08:24 -0700

    rev

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,4 +8,25 @@
 */
 void rev_string(char *s)
 {
-	for (s -= 1 ; s >= 0 ; s--)
+	int i, c, k;
+	char *a, aux;
+
+	a = s;
+
+	while (s[c] != '\0')
+	{
+		c++;
+	}
+	for (k = 1; k < c; k++)
+	{
+		a++;
+	}
+
+	for (i = 0; i < (c / 2); i++)
+	{
+		aux = s[i];
+		s[i] = *a;
+		*a = aux;
+		a--;
+	}
+}
