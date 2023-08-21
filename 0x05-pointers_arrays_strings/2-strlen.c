229d4e7e1cf59d8e6135b234e3865f67f769900a
Areejslama <Areejslama@github.com>
2023-08-21 09:31:41 -0700

    string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * int _strlen- function return the length of string
+ * @s:parameter
+ *
+ * Return:length of string
+*/
+int _strlen(char *s)
+{
+	int str_len(char *s[ ]);
+}
