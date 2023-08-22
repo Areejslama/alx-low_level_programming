5cd759039756e1d38e5a1627e7a93505ae9413a2
Areejslama <Areejslama@github.com>
2023-08-21 17:29:31 -0700

    6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *  puts2- prints every other character of a string
+ *  @str:parameter
+ *
+ *  Return:void
+*/
+void puts2(char *str)
+{
+	int m;
+	for (m = 0 ; str[m] != '\0' ; m++)
+		if ( m % 2 == 0)
+		{
+			_putchar(str[m]);
+		}
+	_putchar('\n');
+}
