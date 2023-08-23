e2f0ba9e37748ca7674d5ef14e5b92cb833c0b9f
Areejslama <Areejslama@github.com>
2023-08-23 12:36:54 -0700

    copy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -4,20 +4,20 @@
  * _strncpy-  function that copies a string
  * @dest:string to be copied
  * @src:string to be copied
- * @n:an integer
+ * @n:number of bytes
  *
- * Return:0 Always
+ * Return:dest
 */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i;
+int i;
 
-	for (i = 0 ; i < n && src[i] != '\0' ; i++)
-			dest[i] = src[i]
-		while (i < n)
-		{
-			dest[i] = '\0';
-			i++;
-		}
-	return (dest);
-}	
+for (i = 0; i < n && src[i] != '\0'; i++)
+dest[i] = src[i];
+while (i < n)
+{
+dest[i] = '\0';
+i++;
+}
+return (dest);
+}
