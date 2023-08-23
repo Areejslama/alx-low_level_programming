e414ff84598afd8cc8806d3490d7066b3c713e82
Areejslama <Areejslama@github.com>
2023-08-23 08:56:53 -0700

    cat2

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -10,14 +10,14 @@
 */
 char *_strncat(char *dest, char *src, int n)
 {
-	int dest_len = 0;
+	int len = strlen(dest);
 	int i;
 
 	for (i = 0 ; i < n && *src != '\0' ; i++)
 	{
-		dest[dest_len+i] = src[i];
+		dest[len + i] = src[i];
 		src++;
 	}
-	dest[dest_len + i] = '\0';
+	dest[len + i] = '\0';
 	return (dest);
 }
