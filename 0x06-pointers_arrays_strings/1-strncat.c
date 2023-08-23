cde72eb285cf5a7001b5ea27903b84e0ece06043
Areejslama <Areejslama@github.com>
2023-08-23 10:04:50 -0700

    cat2

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,6 +1,5 @@
 #include <string.h>
 #include "main.h"
-
 /**
  * _strncat-  function that concatenates two strings
  * @dest:string to be cat
@@ -11,15 +10,15 @@
 */
 char *_strncat(char *dest, char *src, int n)
 {
-	int dest_len = strlen(dest);
+	int len = strlen(dest);
 	int i;
 
 	for (i = 0 ; i < n && *src != '\0' ; i++)
 	{
-		dest[dest_len + i] = *src;
+		dest[len + i] = *src;
 		*src++;
 	}
-	dest[dest_len + i] = '\0';
+	dest[len + i] = '\0';
 	return (dest);
 
 }
