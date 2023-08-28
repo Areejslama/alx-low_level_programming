c2f87e223300b57b2b58d8b4aa59d83a78f3bc92
Areejslama <Areejslama@github.com>
2023-08-28 07:55:25 -0700

    cpy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _memcpy- copies memory area
+ * @dest:pointer 
+ * @src:mermory pointer
+ * @n:an integer
+ *
+ * Return:dest
+*/
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i
+	
+	for (i = 0 ; i < n ; i++)
+	{
+		src[i] = dest[i];
+	}
+	return (dest);
+
+
