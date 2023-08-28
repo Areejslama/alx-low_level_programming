3b8b0103a52f82cbf5f1f546bbcf4e722c268004
Areejslama <Areejslama@github.com>
2023-08-28 13:14:47 -0700

    pbrk

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -12,6 +12,7 @@ char *_strpbrk(char *s, char *accept)
 	int i;
 
 	while (*s)
+	{
 	for (i = 0 ; accept[i] ; i++)
 	{
 		if (*s == accept[i])
@@ -19,6 +20,7 @@ char *_strpbrk(char *s, char *accept)
 			return (s);
 			s++;
 		}
+	}
 	}
 	return (NULL);
 }
