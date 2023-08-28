cdffd46535811749ac092b388f45d9541bd33214
Areejslama <Areejslama@github.com>
2023-08-28 08:38:36 -0700

    str

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _strchr- locates a character in a string
+ * @s:a string
+ * @c:a character
+ *
+ * Return:0 Always
+*/
+char *_strchr(char *s, char c)
+{
+	if (*s != c)
+	{
+		s++;
+	}
+	else
+	{
+		return (s);
+	}
+	if (c == '\0')
+	{
+		return (s);
+	}
+	return (NULL);
+}
