5ed9af1b45cd63bd90d79af1e57a9418f4701658
Areejslama <Areejslama@github.com>
2023-08-30 09:49:16 -0700

    fact

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -8,10 +8,14 @@
 */
 int factorial(int n)
 {
-	if (n == 0)
+	if (n < 0)
 	{
-		return(1);
+		return(-1);
 	}
-	else
-		return (n* factorial(n - 1));
+	if (n == 0);
+	{
+		return (1);
+	}
+	else 
+		return (n*fact (n - 1))
 }
