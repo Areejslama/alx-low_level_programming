e5dadce9b305c46bdb6ef76f624fd49ffa7c6700
Areejslama <Areejslama@github.com>
2023-08-30 16:41:49 -0700

    palin

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -8,11 +8,10 @@ int check_palindrome(char *s);
  * Return:1 or 0
 */
 int is_palindrome(char *s)
-{int is_palindrome(char *s)
 {
 	if (*s == 0)
 		return (1);
-	return (check_pal(s, 0, _strlen_recursion(s)));
+	return (check_palindrome(s, 0, _strlen_recursion(s)));
 }
 
 /**
@@ -29,19 +28,19 @@ int _strlen_recursion(char *s)
 }
 
 /**
- * check_pal - checks the characters recursively for palindrome
+ * check_palindrome - checks the characters recursively for palindrome
  * @s: string to check
  * @i: iterator
  * @len: length of the string
  *
  * Return: 1 if palindrome, 0 if not
  */
-int check_pal(char *s, int i, int len)
+int check_palindrome(char *s, int i, int len)
 {
 	if (*(s + i) != *(s + len - 1))
 		return (0);
 	if (i >= len)
 		return (1);
-	return (check_pal(s, i + 1, len - 1));
+	return (check_palindrome(s, i + 1, len - 1));
 }
 
