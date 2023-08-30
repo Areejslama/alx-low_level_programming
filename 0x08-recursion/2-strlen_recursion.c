6c14906588bceeeecad3a2aaad69d13597919920
Areejslama <Areejslama@github.com>
2023-08-30 08:00:18 -0700

    strlen

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _strlen_recursion- returns the length of a string
+ * @s:a string
+ *
+ * Return:string length
+*/
+int _strlen_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		_strlen_recursion(s + 1);
+	}
+}
