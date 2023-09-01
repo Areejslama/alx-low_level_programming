d4ea96a50c8f37779a20f9c4d23da8e6aa76db61
Areejslama <Areejslama@github.com>
2023-09-01 11:46:25 -0700

    grc

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main- this is entry point
+ * @argc:an integer
+ * @argv:parameter
+ *
+ * Return:0 Always
+*/
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%d\n", argc);
+	return (0);
+}
