eb035568bb6b476780de1b38f463b6aebe8fb68e
Areejslama <Areejslama@github.com>
2023-09-01 11:15:03 -0700

    name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main- this is entry point
+ * @argc:an integer
+ * @argv:a character
+ *
+ * Return:0 Always
+*/
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[])
+		return (0);
+}
