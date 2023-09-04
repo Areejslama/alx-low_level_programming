6c86065ea5183a3a93b209194d6e427aa3d96ec0
Areejslama <Areejslama@github.com>
2023-09-04 15:43:07 -0700

    cat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -9,29 +9,29 @@
 */
 char *str_concat(char *s1, char *s2)
 {
-	int i;
-	int len1 = 0;
-	int len2 = 0;
-	char *a;
+int i;
+int len1 = 0;
+int len2 = 0;
+char *a;
 
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
-	for (i = 0; s1[i] != '\0'; i++)
-		len1++;
-	for (i = 0 ; s2[i] != '\0' ; i++)
-			len2++;
+if (s1 == NULL)
+s1 = "";
+if (s2 == NULL)
+s2 = "";
+for (i = 0; s1[i] != '\0'; i++)
+len1++;
+for (i = 0 ; s2[i] != '\0' ; i++)
+len2++;
 
-	a = malloc((sizeof(char) * (len1 + len2) + 1));
-	if (a == NULL)
-	{
-		return (NULL);
-	}
-	for (i = 0 ; s1[i] ; i++)
-	a[i] = s1[i];
-	for (i = 0 ; s2[i] ; i++)
-	a[len1 + i] = s2[i];
+a = malloc((sizeof(char) * (len1 + len2) +1));
+if (a == NULL)
+{
+return (NULL);
+}
+for (i = 0 ; s1[i] ; i++)
+a[i] = s1[i];
+for (i = 0 ; s2[i] ; i++)
+a[len1 + i] = s2[i];
 
-	return (a);
+return (a);
 }
