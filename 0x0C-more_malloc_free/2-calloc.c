6b8288e812647c74bd09d6d7f4a3a467801a0fc5
Areejslama <Areejslama@github.com>
2023-09-07 13:21:10 -0700

    calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -12,11 +12,11 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	char *a;
 
-	a = calloc(nmemb, size);
 	if (nmemb == 0 || size == 0)
 	{
 		return (NULL);
 	}
+	a = calloc(nmemb, size);
 	if (a == NULL)
 	{
 		return (NULL);
