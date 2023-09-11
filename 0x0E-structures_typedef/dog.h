0b80133f8ee3f58012388a3d2fba44a1532feb91
Areejslama <Areejslama@github.com>
2023-09-11 13:58:47 -0700

    dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * struct dog- new struct
+ *@name:first member
+ *@age:second member
+ *@owner:third member
+ *Description:structs define a new type of members
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
