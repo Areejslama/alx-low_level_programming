5796a9aa8ee31b2370611593defdc46f3d7d683b
Areejslama <Areejslama@github.com>
2023-09-11 18:53:29 -0700

    free

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * free_dog- function that frees dogs
+ * @d:pointer
+ * Return:freed dogs
+*/
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		if (d->name)
+		{
+			free(d->name);
+		}
+		if (d->owner)
+		{
+			free(d->owner);
+		}
+		free(d);
+}	}
