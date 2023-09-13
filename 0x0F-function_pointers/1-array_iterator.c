4e8ccebf55d2806626811bd4a640c7e0032451ac
Areejslama <Areejslama@github.com>
2023-09-13 08:23:58 -0700

    array

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -11,11 +11,10 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i;
 
-	for (i = 0; i < size; i++)
+	for (i = 0; i < size + 1; i++)
 	{
-		size++;
-	}
 	action(array[i]);
+	}
 
 
 }
