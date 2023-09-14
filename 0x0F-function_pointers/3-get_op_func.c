acd901adb6a6d797101ac0acd2a8c2d13de609cf
Areejslama <Areejslama@github.com>
2023-09-13 17:50:56 -0700

    main

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,5 +1,6 @@
 #include "3-calc.h"
 #include <stdlib.h>
+#include <string.h>
 /**
  * get_op_func- function that selects the correct function to perform
  * @s:the opreator
@@ -18,7 +19,13 @@ op_t ops[] = {
 };
 int i = 0;
 
-while (ops[i].op != NULL && *(ops[i].op) != *s)
-	i++;
+while (i < 5)
+{
+if (strcmp(s, ops[i].op) == 0)
+{
 return (ops[i].f);
+}
+i++;
+}
+return (0);
 }
