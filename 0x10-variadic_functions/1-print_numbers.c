9d090236ab5818505114d22782d606426c061be2
Areejslama <Areejslama@github.com>
2023-09-14 12:23:12 -0700

    num

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -18,10 +18,10 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	{
 		num = va_arg(print, int);
 		printf("%d", num);
-	}
-	if (i != n - 1 && separator != NULL)
+	if (i < n - 1 && separator != NULL)
 	{
 		printf("%s", separator);
+	}
 	}
 	va_end(print);
 	printf("\n");
