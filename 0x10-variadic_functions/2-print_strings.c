dcf6eb228bd10a64253f41d874800e5e798e719f
Areejslama <Areejslama@github.com>
2023-09-14 13:52:55 -0700

    string

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -11,13 +11,13 @@ void print_strings(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i;
 	va_list args;
-	const char *s;
+       	char *s;
 
 	va_start(args, n);
 
 	for (i = 0; i < n; i++)
 	{
-		s = va_arg(args, const char *);
+		s = va_arg(args, char *);
 	if (s == NULL)
 	{
 		printf("(nil)");
