b74f470478752f5d9873cd4b373794c354e8e663
Areejslama <Areejslama@github.com>
2023-09-21 14:17:19 -0700

    add

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,26 @@
+#include <string.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * *add_node- adds a new node at the beginning of a list
+ *@head:first node
+ *@str:a string
+ * Return:address or NULL
+*/
+list_t *add_node(list_t **head, const char *str)
+{
+		list_t *node;
+
+		if (head != NULL && str != NULL)
+		{
+		node = malloc(sizeof(list_t));
+		if (node == NULL)
+			return (NULL);
+		node->str = strdup(str);
+		node->len = strlen(str)
+		node->next = *head;
+		 *head = node;
+	return (node);
+		}
+	return (0);
+}
