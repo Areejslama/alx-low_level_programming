1371bfcb57e1fcc5efcd4b3ef35b23e4eada0992
Areejslama <Areejslama@github.com>
2023-09-21 11:18:34 -0700

    list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,17 +8,16 @@
 */
 size_t print_list(const list_t *h)
 {
-size_t num = 0;
+	size_t a = 0;
 
-while (h != NULL)
-{
-if (h->str == NULL)
-printf("[0] (nil)\n");
-
-else
-printf("[%d], %s\n",  h->len, h->str);
-h = h->next;
-num++;
-}
-return (num);
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d], %s\n", h->len, h->str);
+		h = h->next;
+		a++;
+	}
+	return (a);
 }
