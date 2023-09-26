82158b089fa7bb59847bfdd2932be9e388bd680b
Areejslama <Areejslama@github.com>
2023-09-26 15:55:18 -0700

    ind

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+/**
+ * *get_nodeint_at_index-  returns the nth node
+ * @head:pointer
+ * @index:int
+ * Return:null
+*/
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+
+unsigned int count = 0;
+
+if (head)
+{
+while (head)
+{
+if (count == index)
+{
+return (head);
+}
+head = head->next;
+count++;
+}
+return (NULL);
+}
+}
