f1fb04d16202275cd768a833946f8a3995b74792
Areejslama <Areejslama@github.com>
2023-09-26 08:15:07 -0700

    free

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * free_listint- frees a list
+ * @head:pointer
+ * Return:void
+*/
+void free_listint(listint_t *head)
+{
+	listint_t *node;
+
+	if (head != NULL)
+	{
+	node = head;	
+	head = head->next;
+	free(node);
+	}
+}
