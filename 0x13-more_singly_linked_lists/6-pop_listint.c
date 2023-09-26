4cd7124f8d573d02d3cbc7f9bd3020cb2cb8af45
Areejslama <Areejslama@github.com>
2023-09-26 15:33:47 -0700

    del

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * pop_listint- deletes the head node 
+ * @head:pointer
+ * Return:0
+*/
+int pop_listint(listint_t **head)
+{
+listint_t *temp;
+int data;
+
+if (head == NULL)
+{
+return (0);
+}
+temp = *head;
+data = temp->n;
+*head = temp->next;
+free(temp);
+return (data);
+}
