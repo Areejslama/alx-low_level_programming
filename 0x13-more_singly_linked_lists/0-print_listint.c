ce82be6e42f9353d934a18a99aae00efd157702f
Areejslama <Areejslama@github.com>
2023-09-26 13:47:56 -0700

    print

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -7,9 +7,7 @@
 size_t print_listint(const listint_t *h)
 {
 size_t i = 0;
-listint_t *head;
 
-head = malloc(sizeof(listint_t));
 while (h != NULL)
 {
 if (head == NULL)
@@ -22,7 +20,6 @@ printf("%d\n",  h->n);
 i++;
 h = h->next;
 }
-free(head);
 }
 return (i);
 }
