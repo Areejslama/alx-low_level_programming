c0befa1275c1121f2612e6872c78e4d2889b4658
Areejslama <Areejslama@github.com>
2023-10-02 17:21:57 -0700

    create

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,19 +14,13 @@ int create_file(const char *filename, char *text_content)
 {
 	int fd;
 
-	if (filename == NULL)
-	{
+	if (!filename)
 		return (-1);
-	}
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC,  S_IRUSR | S_IWUSR);
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (fd == -1)
-	{
 		return (-1);
-	}
-	if (text_content == NULL)
-	{
-		write(fd, text_content, strlen(text_content));
+	if (text_content)
+		write(fd, text_content, _strlen(text_content));
 	close(fd);
-	}
 	return (1);
 }
