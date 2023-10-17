86b442c475141ee8758dbdaa62847810a40a0928
Areejslama <Areejslama@github.com>
2023-10-17 16:02:07 -0700

    cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,67 +1,73 @@
 #include "main.h"
 #include <stdlib.h>
-/**
- * exit_with_error - Print an error message and exit with the given exit code.
- * @exit_code: The exit code to use.
- * @format: The format string for the error message.
- * @...: Additional arguments to the format string.
- */
-void exit_with_error(int exit_code, const char *format, ...)
-{
-	va_list args;
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <unistd.h>
 
-	va_start(args, format);
-
-	dprintf(STDERR_FILENO, format, args);
-	va_end(args);
-	exit(exit_code);
-}
+void check_IO_stat(int stat, int fd, char *filename, char mode);
 /**
- * main - Copy the content of one file to another.
- * @argc: The number of command-line arguments.
- * @argv: An array of command-line arguments.
- * Return: 0 on success, other values on error.
+ * main - copies the content of one file to another
+ * @argc: argument count
+ * @argv: arguments passed
+ *
+ * Return: 1 on success, exit otherwise
  */
 int main(int argc, char *argv[])
 {
-	int from_fd, to_fd;
-	ssize_t bytes_read, bytes_written;
-	char buffer[BUFFER_SIZE];
+	int src, dest, n_read = 1024, wrote, close_src, close_dest;
+	unsigned int mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
+	char buffer[1024];
 
 	if (argc != 3)
 	{
-		exit_with_error(97, "Usage: cp file_from file_to\n");
+		dprintf(STDERR_FILENO, "%s", "Usage: cp file_from file_to\n");
+		exit(97);
 	}
-	from_fd = open(argv[1], O_RDONLY);
-	if (from_fd == -1)
+	src = open(argv[1], O_RDONLY);
+	check_IO_stat(src, -1, argv[1], 'O');
+	dest = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, mode);
+	check_IO_stat(dest, -1, argv[2], 'W');
+	while (n_read == 1024)
 	{
-		exit_with_error(98, "Error: Can't read from file %s\n", argv[1]);
+		n_read = read(src, buffer, sizeof(buffer));
+		if (n_read == -1)
+			check_IO_stat(-1, -1, argv[1], 'O');
+		wrote = write(dest, buffer, n_read);
+		if (wrote == -1)
+			check_IO_stat(-1, -1, argv[2], 'W');
 	}
-	to_fd = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR |
-		S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
-	if (to_fd == -1)
-	{
-		close(from_fd);
-		exit_with_error(99, "Error: Can't write to file %s\n", argv[2]);
-	}
-	while ((bytes_read = read(from_fd, buffer, BUFFER_SIZE)) > 0)
-	{
-		bytes_written = write(to_fd, buffer, bytes_read);
-		if (bytes_written != bytes_read)
-		{
-			close(from_fd);
-			close(to_fd);
-			exit_with_error(99, "Error: Can't write to file %s\n", argv[2]);
-		}
-	}
-	if (bytes_read == -1)
-	{
-		close(from_fd);
-		close(to_fd);
-		exit_with_error(98, "Error: Can't read from file %s\n", argv[1]); }
-	if (close(from_fd) == -1 || close(to_fd) == -1)
-	{
-		exit_with_error(100, "Error: Can't close fd\n");
-	}
+	close_src = close(src);
+	check_IO_stat(close_src, src, NULL, 'C');
+	close_dest = close(dest);
+	check_IO_stat(close_dest, dest, NULL, 'C');
 	return (0);
+}
+
+/**
+ * check_IO_stat - checks if a file can be opened or closed
+ * @stat: file descriptor of the file to be opened
+ * @filename: name of the file
+ * @mode: closing or opening
+ * @fd: file descriptor
+ *
+ * Return: void
+ */
+void check_IO_stat(int stat, int fd, char *filename, char mode)
+{
+	if (mode == 'C' && stat == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
+	else if (mode == 'O' && stat == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", filename);
+		exit(98);
+	}
+	else if (mode == 'W' && stat == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", filename);
+		exit(99);
+	}
 }
