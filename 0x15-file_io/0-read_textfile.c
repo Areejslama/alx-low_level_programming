3df74f1e68f05139ec0973fc731f0455626f95a6
Areejslama <Areegahmed449@gmail.com>
2023-11-28 14:59:00 -0800

    text

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -11,29 +11,28 @@
 */
 ssize_t read_textfile(const char *filename, size_t letters)
 {       
-        size_t fd;
-        char *file;
-        size_t let;
-	size_t f;
+        ssize_t let;
+	ssize_t f, r;
+	char *file;
         
 	file = malloc(letters);
 	if (file == NULL)
 	{
-		free(file);
 		return (0);
 	}
 	if (filename == NULL)
 	{
 		return (0);
 	}
-        fd = open(filename, O_RDONLY);
-        if (fd == - 1)
-        {       
+        r = open(filename, O_RDONLY);
+        if (r == -1)
+        {
+		free(file);
                 return (0);
         }       
-        let = read(fd, file, letters);
+        let = read(r, file, letters);
 	f = write(STDIN_FILENO, file, let);
         
-	close(fd);
+	close(r);
 	return (f);
 }
