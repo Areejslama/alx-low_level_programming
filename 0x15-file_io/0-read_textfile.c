c8965b867e20e0f062ac27a7b8f808d65c6ac839
Areejslama <Areegahmed449@gmail.com>
2023-11-28 14:15:21 -0800

    text

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include<unistd.h>
+#include<fcntl.h>
+#include "main.h"
+#include <stdlib.h>
+/**
+ *  read_textfile- read the file 
+ *  @filename:name of file
+ *  @letters:letters to be printed
+ *  Return:0 Always
+*/
+ssize_t read_textfile(const char *filename, size_t letters)
+{       
+        int fd;
+        char *file;
+        size_t let;
+	size_t f;
+        
+        if (filename == NULL)
+        {       
+                return (0);
+        }
+	file = malloc(letters);
+	if (file == NULL)
+	{
+		free(file);
+		return (0);
+	}
+        fd = open(filename, O_RDONLY);
+        if (fd == - 1)
+        {       
+                return (0);
+        }       
+        let = read(fd, file, letters);
+	f = write(STDIN_FILENO, file, let);
+        
+	close(fd);
+	return (f);
+}
