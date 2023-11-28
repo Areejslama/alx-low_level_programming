28b681e91cd7328073d7bb06ef45a2ef8f4d3c8f
Areejslama <Areegahmed449@gmail.com>
2023-11-28 15:17:56 -0800

    text

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -4,17 +4,17 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- *  read_textfile- read the file 
+ *  read_textfile- read the file
  *  @filename:name of file
  *  @letters:letters to be printed
  *  Return:0 Always
 */
 ssize_t read_textfile(const char *filename, size_t letters)
-{       
-        ssize_t let;
+{
+	ssize_t let;
 	ssize_t f, r;
 	char *file;
-        
+
 	file = malloc(letters);
 	if (file == NULL)
 	{
@@ -24,15 +24,14 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	{
 		return (0);
 	}
-        r = open(filename, O_RDONLY);
-        if (r == -1)
-        {
+	r = open(filename, O_RDONLY);
+	if (r == -1)
+	{
 		free(file);
-                return (0);
-        }       
-        let = read(r, file, letters);
+		return (0);
+	}
+	let = read(r, file, letters);
 	f = write(STDIN_FILENO, file, let);
-        
 	close(r);
 	return (f);
 }
