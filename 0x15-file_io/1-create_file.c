2c077c06030d54c5654445f1a453ec9d1ca3301e
Areejslama <Areegahmed449@gmail.com>
2023-11-29 12:01:39 -0800

    create

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -7,7 +7,7 @@
 /**
  * create_file- create a file
  * @filename:name of file
- * @text content:the content
+ * @text_content:the content
  * Return:0 Always
 */
 int create_file(const char *filename, char *text_content)
@@ -20,12 +20,12 @@ int create_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
-	file = open(filename, O_CREAT| O_TRUNC| O_WRONLY, 0600);
+	file = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
 	if (file == -1)
 	{
 		return (-1);
 	}
-	if(text_content != NULL)
+	if (text_content != NULL)
 	{
 	n = write(file, text_content, strlen(text_content));
 	if (n == -1)
