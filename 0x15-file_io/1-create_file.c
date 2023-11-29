3cbdbdcc1613b4edd8208ce9aa907256ebd89588
Areejslama <Areegahmed449@gmail.com>
2023-11-29 11:40:13 -0800

    create

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -22,14 +22,14 @@ int create_file(const char *filename, char *text_content)
 	{
 		text_content = "";
 	}
-	file = open(filename, O_CREATE, O_TRUNC, 0644);
+	while ( text_content[n] != '\0')
+	{
+		n++;
+	}
+	file = open(filename, O_WRONLY| O_CREAT| O_TRUNC, 6000);
 	if (file == -1)
 	{
 		return (-1);
-	}
-	while ( text_content != '\0')
-	{
-		n++;
 	}
         write(file, text_content, n);
 	return (1);
