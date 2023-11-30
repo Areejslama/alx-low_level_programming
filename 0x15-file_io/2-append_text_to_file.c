adda155f39cebc8ecfceb55cb0da472676723084
Areejslama <Areegahmed449@gmail.com>
2023-11-30 05:03:07 -0800

    hello

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -6,7 +6,7 @@
 /**
  * append_text_to_file- appends text at the end of a file
  * @filename:name of file
- * text_content:content of file
+ * @text_content:content of file
  * Return:1 on success -1 on failure
 */
 int append_text_to_file(const char *filename, char *text_content)
