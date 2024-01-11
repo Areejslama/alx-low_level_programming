e152cc9e4b9e75e599b1f6f7cc9e3248ec6e9e9c
Areejslama <Areegahmed449@gmail.com>
2024-01-11 11:35:31 -0800

    len

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "lists.h"
+/**
+ * dlistint_len- get length of list
+ * @h:pointer
+ *
+ * Return:0
+*/
 size_t dlistint_len(const dlistint_t *h)
 {
 	size_t n = 0;
-	
+
 	while (h != NULL)
 	{
 		n++;
