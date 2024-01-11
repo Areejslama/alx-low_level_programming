71b8b70861dde5eb2d54b3bb08f4bc2482a75ef6
Areejslama <Areegahmed449@gmail.com>
2024-01-11 10:59:07 -0800

    list

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * print_dlistint- print a list of elements
+ * @h:pointer
+ *
+ * Return:0 Always
+*/
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t i = 0;
+
+	if (h != NULL)
+	{
+		printf("%d\n", h->n)
+		i++;
+		h = h->next;
+	}
+	return(i)
+}
