51dae4d7a46f2dba9d54e27fe49d2c9ff2cef6c4
Areejslama <Areegahmed449@gmail.com>
2024-01-11 11:22:04 -0800

    list

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -11,11 +11,11 @@ size_t print_dlistint(const dlistint_t *h)
 {
 	size_t i = 0;
 
-	if (h != NULL)
+	while (h != NULL)
 	{
 		printf("%d\n", h->n);
-		i++;
 		h = h->next;
+		i++;
 	}
-	return(i);
+	return (i);
 }
