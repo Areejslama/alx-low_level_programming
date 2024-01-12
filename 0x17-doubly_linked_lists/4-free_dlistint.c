18f0d2c1c8a535a947725f4da02d7a4a8c49c91c
Areejslama <Areegahmed449@gmail.com>
2024-01-11 16:53:33 -0800

    free

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * free_dlistint - free memory
+ * @head: haed of nodes
+ * Return: values
+ */
+void free_dlistint(dlistint_t *head)
+
+{
+	dlistint_t *current = head;
+
+	while (current != NULL)
+	{
+		dlistint_t *next = current->next;
+
+		free(current);
+		current = next;
+	}
+
+	head = (NULL);
+}
