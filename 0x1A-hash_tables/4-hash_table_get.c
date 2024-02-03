0957ce77d123cb4e11db82c6c777caa3c5f79af3
Areejslama <Areegahmed449@gmail.com>
2024-02-02 17:42:02 -0800

    get

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -13,25 +13,19 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	char *compare_key;
 	unsigned long int index;
 	hash_node_t *current_node;
-	if (*key == '\0')
+
+	 if (key == '\0' || key == NULL || ht == NULL)
+		return (NULL);
+	 index = key_index((const unsigned char *)key, ht->size);
+	 current_node = ht->array[index];
+	while (current_node)
 	{
-        return (NULL);
+	compare_key = current_node->key;
+	if (strcmp(compare_key, key) == 0)
+	{
+		return (current_node->value);
 	}
-
-    index = key_index((const unsigned char *)key, ht->size);
-    current_node = ht->array[index];
-
-    while (current_node)
-    {
-	    compare_key = current_node->key;
-	    
-	    if (strcmp(compare_key, key) == 0)
-	    {
-            return (current_node->value);
-	    }
-	    
-	    current_node = current_node->next;
-    }
-    return (NULL);
+	current_node = current_node->next;
+	}
+	return (NULL);
 }
-
